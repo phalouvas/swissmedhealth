48a1c3d9efed6c62082d9a7ffe408e3619959dd7
Panayiotis Halouvas <phalouvas@kainotomo.com>
2024-01-16 04:45:51 +0000

    Additional customization Fixes #31

--- swissmedhealth/swissmedhealth/web_form/contact_us/contact_us.js
+++ swissmedhealth/swissmedhealth/web_form/contact_us/contact_us.js
@@ -3,7 +3,11 @@ frappe.ready(function() {
 	// bind events here
 	frappe.web_form.after_save = () => {
 		let email_id = frappe.web_form.doc.email_id;
-		window.location.href = '../lead-step-2/new?email_id=' + encodeURIComponent(email_id);
+		let href = '/lead-step-2/new?email_id=' + encodeURIComponent(email_id);
+		// Get the redirect button with class name "new-btn" and set the href attribute
+		$('.new-btn').attr('href', href);
+		// Also change the text of the button to "Please complete your medical history here"
+		$('.new-btn').text('Please complete your medical history here');
 	}
 
 })
