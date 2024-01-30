7e6cebef7165cb7b738447c3ed952d0a572491c1
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-30 03:27:22 +0000

    New section : Dental History Fixes #58

--- swissmedhealth/public/js/lead.js
+++ swissmedhealth/public/js/lead.js
@@ -395,10 +395,5 @@ frappe.ui.form.on("Lead", {
 			}, __('Action'));
 
 		}
-
-		// if field custom_date is empty set the current date
-		if (!frm.doc.custom_date) {
-			frm.set_value("custom_date", frappe.datetime.nowdate());
-		}
 	}
 });
--- swissmedhealth/swissmedhealth/doctype/customer_consent/customer_consent.js
+++ swissmedhealth/swissmedhealth/doctype/customer_consent/customer_consent.js
@@ -2,7 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Customer Consent', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		// if field custom_date is empty set the current date
+		if (!frm.doc.acceptance_date) {
+			frm.set_value("acceptance_date", frappe.datetime.nowdate());
+		}
+	}
 });
