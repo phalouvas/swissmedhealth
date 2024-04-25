cc182e0595330492b4a834a7f04c0f67b16f40d2
Panayiotis Halouvas <phalouvas@gmail.com>
2024-04-25 04:09:34 +0000

    Refactor contact_us.js to use async/await for submit button click event

--- swissmedhealth/swissmedhealth/web_form/contact_us/contact_us.js
+++ swissmedhealth/swissmedhealth/web_form/contact_us/contact_us.js
@@ -22,7 +22,7 @@ frappe.ready(function () {
 		$('.new-btn').text('Medical information Form');
 	}
 
-	$('.submit-btn').on('click', function (e) {
+	$('.submit-btn').on('click', async function (e) {
 
 		// Get the email from the URL
 		let params = new URLSearchParams(window.location.search);
@@ -30,31 +30,27 @@ frappe.ready(function () {
 		if (!email_id) {
 			// Get the email_id from the submitted form
 			email_id = frappe.web_form.doc.email_id;
-			frappe.call('swissmedhealth.swissmedhealth.web_form.medical_history.medical_history.get_lead_details', { email_id: email_id, throw_error: false }).then(r => {
-				if (r.message == undefined) {
-					// enable submission of form
+			let r = await frappe.call('swissmedhealth.swissmedhealth.web_form.medical_history.medical_history.get_lead_details', { email_id: email_id, throw_error: false });
+			if (r.message == undefined) {
+				return;
+			}
+			frappe.web_form.doc.name = r.message.name;
+			// Prevent the default form submission
+			e.preventDefault();
 
-					return;
-				}
-				frappe.web_form.doc.name = r.message.name;
-				// Prevent the default form submission
-				e.preventDefault();
-
-				frappe.call('swissmedhealth.swissmedhealth.web_form.medical_history.medical_history.save', { doc: frappe.web_form.doc }).then(r => {
-					let email_id = r.message.email_id;
-					window.location.href = '../lead-step-2/new?email_id=' + encodeURIComponent(email_id);
-				}).catch((err) => {
-					frappe.msgprint({
-						title: __('Error'),
-						indicator: 'red',
-						message: __('An error occurred while submitting your details. Please try again later.')
-					});
+			frappe.call('swissmedhealth.swissmedhealth.web_form.medical_history.medical_history.save', { doc: frappe.web_form.doc }).then(r => {
+				let email_id = r.message.email_id;
+				window.location.href = '../lead-step-2/new?email_id=' + encodeURIComponent(email_id);
+			}).catch((err) => {
+				frappe.msgprint({
+					title: __('Error'),
+					indicator: 'red',
+					message: __('An error occurred while submitting your details. Please try again later.')
 				});
 			});
 		} else {
 			// Prevent the default form submission
 			e.preventDefault();
-
 			frappe.call('swissmedhealth.swissmedhealth.web_form.medical_history.medical_history.save', { doc: frappe.web_form.doc }).then(r => {
 				let email_id = r.message.email_id;
 				window.location.href = '../lead-step-2/new?email_id=' + encodeURIComponent(email_id);
