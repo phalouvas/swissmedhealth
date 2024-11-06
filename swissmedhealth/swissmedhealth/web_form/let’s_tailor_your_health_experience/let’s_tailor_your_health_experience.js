f4e8d8df7a6a0cf15a1d4a7daca11b03363247e5
Panayiotis Halouvas <phalouvas@gmail.com>
2024-11-06 07:59:47 +0000

    Update success message and hide web form after submission in health experience form

--- swissmedhealth/swissmedhealth/web_form/let’s_tailor_your_health_experience/let’s_tailor_your_health_experience.js
+++ swissmedhealth/swissmedhealth/web_form/let’s_tailor_your_health_experience/let’s_tailor_your_health_experience.js
@@ -25,12 +25,13 @@ frappe.ready(function () {
 
 	// bind events here
 	frappe.web_form.after_save = () => {
-		let email_id = frappe.web_form.doc.email_id;
-		let href = '/partners-step-2/new?email_id=' + encodeURIComponent(email_id);
-		// Get the redirect button with class name "new-btn" and set the href attribute
-		$('.new-btn').attr('href', href);
-		// Also change the text of the button to "Please complete your medical history here"
-		$('.new-btn').text('Health Goals Form');
+		frappe.msgprint({
+			title: __('Success'),
+			indicator: 'green',
+			message: __('We have received your information and a member of our patient management team will be contacting you via your preferred contact method shortly.')
+		});
+
+		$('.web-form-container').hide();
 	}
 
 	$('.submit-btn').on('click', async function (e) {
