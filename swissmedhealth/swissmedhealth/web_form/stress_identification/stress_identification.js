14248a1a7b4ec7017751c0499128431d05cdcef9
Panayiotis Halouvas <phalouvas@gmail.com>
2024-12-11 04:57:23 +0000

    Update scored.json labels and descriptions for clarity; modify redirect behavior in stress_identification.js

--- swissmedhealth/swissmedhealth/web_form/stress_identification/stress_identification.js
+++ swissmedhealth/swissmedhealth/web_form/stress_identification/stress_identification.js
@@ -26,11 +26,7 @@ frappe.ready(function () {
 	// bind events here
 	frappe.web_form.after_save = () => {
 		let email_id = frappe.web_form.doc.email_id;
-		let href = '/stress-identification-2/new?email_id=' + encodeURIComponent(email_id);
-		// Get the redirect button with class name "new-btn" and set the href attribute
-		$('.new-btn').attr('href', href);
-		// Also change the text of the button to "Please complete your medical history here"
-		$('.new-btn').text('Medical information Form');
+		window.location.href = '/stress-identification-2/new?email_id=' + encodeURIComponent(email_id);
 	}
 
 	$('.submit-btn').on('click', async function (e) {
