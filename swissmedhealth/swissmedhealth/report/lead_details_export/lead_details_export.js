0e132b50d8b8d59595b4261adfbc721c67093a11
Panayiotis Halouvas <phalouvas@gmail.com>
2025-07-21 07:57:26 +0000

    Add initial implementation for Lead Details Export report

---
+++ swissmedhealth/swissmedhealth/report/lead_details_export/lead_details_export.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, KAINOTOMO PH LTD and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Lead Details Export"] = {
+	"filters": [
+
+	]
+};
